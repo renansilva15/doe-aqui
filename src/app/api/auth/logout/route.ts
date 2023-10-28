9b6b9782907a25af9441fb048e55fea2904575b9
renansilva15 <renangabrielsilva150@gmail.com>
2023-10-28 12:57:32 -0300

    refactor: change logout method to POST

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export async function GET() {
+export async function POST() {
   const response = new NextResponse(JSON.stringify({ status: 'success' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
