f510a432f292bcfe11fe1984ef1af5e34874c966
renansilva15 <renangabrielsilva150@gmail.com>
2023-12-31 10:07:31 -0300

    chore: unstaged changes for pull rebase

---
+++ src/app/api/campaign/[id]/route.ts
@@ -0,0 +1,37 @@
+import { getErrorResponse } from '@/lib/helpers'
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const userId = req.headers.get('X-USER-ID')
+
+  if (!userId) {
+    return getErrorResponse(
+      401,
+      'You are not logged in, please provide token to gain access',
+    )
+  }
+
+  const { id } = params
+
+  try {
+    const deletedCampaign = await prisma.campaign.delete({
+      where: { id, userId },
+    })
+
+    return NextResponse.json({
+      status: 'success',
+      data: { deletedCampaign },
+    })
+  } catch (error) {
+    console.error('Error deleting campaign:', error)
+
+    return NextResponse.json({
+      status: 'error',
+      message: 'Failed to delete campaign',
+    })
+  }
+}
