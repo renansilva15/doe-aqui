857f49616aca97d7f197f8e9325a939abadeb019
renansilva15 <renangabrielsilva150@gmail.com>
2024-01-02 11:56:57 -0300

    feat: add update campaign route

--- src/app/api/campaign/[id]/route.ts
+++ src/app/api/campaign/[id]/route.ts
@@ -1,5 +1,9 @@
 import { getErrorResponse } from '@/lib/helpers'
 import { prisma } from '@/lib/prisma'
+import {
+  UpdateCampaignInput,
+  UpdateCampaignSchema,
+} from '@/lib/validations/campaign.schema'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function DELETE(
@@ -33,6 +37,75 @@ export async function DELETE(
     return NextResponse.json({
       status: 'success',
       data: { deletedCampaign },
+    })
+  } catch (error) {
+    console.error('Error deleting campaign:', error)
+
+    return NextResponse.json({
+      status: 'error',
+      message: 'Failed to delete campaign',
+    })
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const userId = req.headers.get('X-USER-ID')
+
+  if (!userId) {
+    return getErrorResponse(
+      401,
+      'You are not logged in, please provide token to gain access',
+    )
+  }
+
+  const { id } = params
+
+  const body = (await req.json()) as UpdateCampaignInput
+
+  const { title, description, goal, totalRaised, pixKey } =
+    UpdateCampaignSchema.parse(body)
+
+  if (!title || !description || !pixKey) {
+    return NextResponse.json({
+      status: 'fail',
+      data: {},
+    })
+  }
+
+  try {
+    const campaign = await prisma.campaign.findUnique({
+      where: { id, userId },
+    })
+
+    if (!campaign) {
+      return getErrorResponse(404, 'Campaign not found')
+    }
+
+    // campaign.title = title
+    // campaign.description = description
+    // campaign.goal = goal as unknown as Decimal
+    // campaign.totalRaised = totalRaised as unknown as Decimal
+    // campaign.pixKey = pixKey
+
+    await prisma.campaign.update({
+      data: {
+        title,
+        description,
+        goal,
+        totalRaised,
+        pixKey,
+      },
+      where: {
+        id,
+      },
+    })
+
+    return NextResponse.json({
+      status: 'success',
+      data: { campaign },
     })
   } catch (error) {
     console.error('Error deleting campaign:', error)
--- src/lib/validations/campaign.schema.ts
+++ src/lib/validations/campaign.schema.ts
@@ -18,20 +18,33 @@ export const RegisterCampaignSchema = z.object({
     .min(1, 'Pix Key must not be empty'),
 })
 
-// export const LoginUserSchema = z.object({
-//   email: z
-//     .string({
-//       required_error: 'Email is required',
-//     })
-//     .min(1, 'Email is required')
-//     .email('Email is invalid'),
-//   password: z
-//     .string({
-//       required_error: 'Password is required',
-//     })
-//     .min(1, 'Password is required')
-//     .min(8, 'Password must be at least 8 characters'),
-// })
+export const UpdateCampaignSchema = z.object({
+  title: z
+    .string({
+      required_error: 'Title is required',
+    })
+    .min(1, 'Title must not be empty'),
+  description: z
+    .string({
+      required_error: 'Description is required',
+    })
+    .min(1, 'Description must not be empty'),
+  goal: z
+    .number({
+      required_error: 'Goal is required',
+    })
+    .min(0, 'Goal must be a positive number'),
+  totalRaised: z
+    .number({
+      required_error: 'Total Raised is required',
+    })
+    .min(0, 'Total Raised must be a positive number'),
+  pixKey: z
+    .string({
+      required_error: 'Pix Key is required',
+    })
+    .min(1, 'Pix Key must not be empty'),
+})
 
-// export type LoginUserInput = z.infer<typeof LoginUserSchema>
 export type RegisterCampaignInput = z.infer<typeof RegisterCampaignSchema>
+export type UpdateCampaignInput = z.infer<typeof UpdateCampaignSchema>
