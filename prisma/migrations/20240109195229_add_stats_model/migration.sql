d0c25614d878864a058df8822b3f232e57f6a788
renansilva15 <renangabrielsilva150@gmail.com>
2024-01-10 13:51:44 -0300

    fix: initialize stats table

--- prisma/migrations/20240109195229_add_stats_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Stats" (
-    "id" SERIAL NOT NULL,
-    "total_registered_campaigns" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "Stats_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20240110154455_initial_migrate/migration.sql
+++ prisma/migrations/20240110154455_initial_migrate/migration.sql
@@ -30,6 +30,14 @@ CREATE TABLE "Pix" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- CreateTable
+CREATE TABLE "Stats" (
+    "id" SERIAL NOT NULL,
+    "total_registered_campaigns" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Stats_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -38,3 +46,6 @@ CREATE UNIQUE INDEX "Pix_tx_id_key" ON "Pix"("tx_id");
 
 -- AddForeignKey
 ALTER TABLE "Campaign" ADD CONSTRAINT "Campaign_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- InsertData
+INSERT INTO "Stats" DEFAULT VALUES;
